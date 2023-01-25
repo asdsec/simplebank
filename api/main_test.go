6739dd1a77d312db5343a8fd63a0e328421c9390
sametdmr <sametdemir1049@gmail.com>
2023-01-25 14:17:44 +0300

    user login has been handled

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,25 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	db "github.com/sametdmr/simplebank/db/sqlc"
+	"github.com/sametdmr/simplebank/utility"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := utility.Config{
+		TokenSymmetricKey:   utility.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
